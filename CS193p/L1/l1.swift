30c35bf921e4826e3e3b7affacfd3639a87d7a5e
Hunter <hunterweisenbach@me.com>
2025-12-09 16:44:12 -0500

    updated L1.swift with class practices

--- CS193p/L1/l1.swift
+++ CS193p/L1/l1.swift
@@ -1,6 +1,30 @@
-let cast = ["Vivien", "Marlon", "Kim", "Karl"]
+class Idol {
+    var kpopBand: String
+    var idolName: String
+    var age: Int
+ 
 
-// Using shorthand closure arguments
-let thirdName = cast.sorted (by: { $0 < $1 })
+    init(kpopBand: String, idolName: String, age: Int){
+        self.kpopBand = kpopBand
+        self.idolName = idolName
+        self.age = age
+    }
 
-print(thirdName)
+    func greeting() -> String {
+        return "This is \(idolName), they are in \(kpopBand), and \(age)"
+    }
+}
+
+
+class Jennie: Idol {
+    init() {
+        super.init(kpopBand: "Blackpink", idolName: "Jennie", age: 67)
+    }
+}
+var j = Jennie()
+print(j.greeting())
+
+//
+var jisoo = Idol(kpopBand: "Blackpink", idolName: "Jisoo", age: 41)
+
+print(jisoo.greeting())
