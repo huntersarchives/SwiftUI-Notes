882fb41763b252146fc2e9a76404136d36ee1546
Hunter <hunterweisenbach@me.com>
2025-12-10 11:38:54 -0500

    updated l2 swift

---
+++ CS193p/L2/l2.swift
@@ -0,0 +1,13 @@
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+   return false
+}
+print(anyCommonElements([1, 2, 5, 5 , 9], [9]))
